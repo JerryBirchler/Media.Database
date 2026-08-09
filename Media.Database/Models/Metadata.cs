5c2c1d2dc420996aeccc759098de984a18753660
Jerry Birchler <jerry.birchler@icloud.com>
2026-08-09 18:02:57 -0500

    MEDIA-5: sync database

--- Media.Database/Models/Metadata.cs
+++ Media.Database/Models/Metadata.cs
@@ -1,11 +1,31 @@
-﻿namespace Media.Database.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace Media.Database.Models;
 
 public class Metadata
 {
+    [JsonPropertyName("keyWords")]
+    [FromHeader(Name = "keyWords")]
     public SortedSet<string>? KeyWords { get; set; } = null;
+
+    [JsonPropertyName("names")]
+    [FromHeader(Name = "names")]
     public SortedSet<string>? Names { get; set; } = null;
+    
+    [JsonPropertyName("title")]
+    [FromHeader(Name = "title")]
     public string? Title { get; set; }
+    
+    [JsonPropertyName("description")]
+    [FromHeader(Name = "description")]
     public string? Description { get; set; }
+
+    [JsonPropertyName("event")]
+    [FromHeader(Name = "event")]
     public string? Event { get; set; }
+
+    [JsonPropertyName("location")]
+    [FromHeader(Name = "location")]
     public string? Location { get; set; }        
 }
